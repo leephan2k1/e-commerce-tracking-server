7f91850212a32c9b93a350ff51b8674b8c1595a7
leephan2k1 <pttl2k1@gmail.com>
2022-08-30 11:40:20 +0700

    feat: 🎸 complete shopee search

--- src/models/Shopee.model.js
+++ src/models/Shopee.model.js
@@ -41,12 +41,25 @@ export async function search(keyword, pageParam) {
                             : `${minPrice} - ${maxPrice}`;
                 }
 
+                const link = `${SHOPEE_URL}/${String(name)
+                    .replace(/\s/g, '-')
+                    .replace(/\[|\]/g, '-')
+                    .replace('%', '')}-i.${product?.item_basic.shopid}.${
+                    product?.item_basic.itemid
+                }`;
+
                 const totalSales = product?.item_basic.historical_sold
                     ? `Đã bán ${product?.item_basic.historical_sold}`
                     : null;
 
-                if (name && product?.item_basic.image && totalSales) {
-                    result.push({ name, img, price, totalSales });
+                if (
+                    name &&
+                    product?.item_basic.image &&
+                    totalSales &&
+                    product?.item_basic.shopid &&
+                    product?.item_basic.itemid
+                ) {
+                    result.push({ name, img, price, totalSales, link });
                 }
 
                 return result;
@@ -58,57 +71,4 @@ export async function search(keyword, pageParam) {
         console.error('error:: ', error);
         return null;
     }
-
-    // try {
-    //     const browser = await puppeteer.launch({ headless: true });
-    //     const page = await browser.newPage();
-    //     await page.goto(
-    //         `${SHOPEE_URL}/search?keyword=${encodeURI(keyword)}${pageNumber}`,
-    //         {
-    //             waitUntil: 'load',
-    //         },
-    //     );
-
-    //     await page.setViewport({
-    //         width: 1200,
-    //         height: 800,
-    //     });
-
-    //     await autoScroll(page);
-
-    //     const products = await page.$$eval(
-    //         '.shopee-search-item-result__item',
-    //         (dom) => {
-    //             const productList = dom.map((aTag) => {
-    //                 const name = aTag
-    //                     .querySelector('.Cve6sh')
-    //                     ?.textContent.trim();
-
-    //                 const price = aTag
-    //                     .querySelector('.ZEgDH9')
-    //                     ?.textContent.trim();
-
-    //                 const img = aTag
-    //                     .querySelector('._7DTxhh.vc8g9F')
-    //                     ?.getAttribute('src');
-
-    //                 const salesTotal = aTag
-    //                     .querySelector('.r6HknA.uEPGHT')
-    //                     ?.textContent.trim();
-
-    //                 return { name, price, img, salesTotal };
-    //             });
-
-    //             if (productList) return productList;
-    //             return [];
-    //         },
-    //     );
-
-    //     if (products) return products;
-
-    //     return null;
-    // } catch (error) {
-    //     console.error('error::: ', error);
-    //     return null;
-    // }
 }
